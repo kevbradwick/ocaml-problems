3a5a2f420d3b3f342a50d8c2516dfc99422aff10
Kevin Bradwick <kevinbradwick@gmail.com>
2023-08-07 22:21:46 +0100

    added recursive implementation of last

--- lib/lists.ml
+++ lib/lists.ml
@@ -3,7 +3,18 @@ let last lst = match List.rev lst with
   | [] -> None
   | h :: _  -> Some h
 
+(* same problem but using recursion *)
+let rec lastr = function
+  | [] -> None
+  | [a] -> Some a
+  | _ :: t -> lastr t
+
 let%test "test last function returns last element of list" =
   last [] = None &&
   last [1;2;3] = Some 3 &&
   last ["a"; "b"] = Some "b"
+
+let%test "test last with recursion" =
+  lastr [] = None &&
+  lastr [1;2;3] = Some 3 &&
+  lastr ["a"; "b"] = Some "b"
