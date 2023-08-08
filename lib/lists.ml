b035aebac441a3d120edcddf62eb3c66b1a0c666
Kevin Bradwick <kevinbradwick@gmail.com>
2023-08-08 08:42:37 +0100

    added solution for last_two

--- lib/lists.ml
+++ lib/lists.ml
@@ -18,3 +18,19 @@ let%test "test last with recursion" =
   lastr [] = None &&
   lastr [1;2;3] = Some 3 &&
   lastr ["a"; "b"] = Some "b"
+
+(*
+   Last Two elements of a list
+
+   Recursion works here because you can pattern match the list with exactly two
+   elements.
+*)
+let rec last_two = function
+  | [] | [_] -> None
+  | [a; b] -> Some (a, b)
+  | _ :: t -> last_two t
+
+let%test "last two with recursion" =
+  last_two [] = None &&
+  last_two [1] = None &&
+  last_two [1;2;3;4] = Some (3, 4)
