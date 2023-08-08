b38005fea5916139c6db50e48e0d33adda30db8b
Kevin Bradwick <kevinbradwick@gmail.com>
2023-08-08 13:25:45 +0100

    added nth and added formatting

--- lib/lists.ml
+++ lib/lists.ml
@@ -1,23 +1,14 @@
 (* return the last elment of a list *)
-let last lst = match List.rev lst with
-  | [] -> None
-  | h :: _  -> Some h
+let last lst = match List.rev lst with [] -> None | h :: _ -> Some h
 
 (* same problem but using recursion *)
-let rec lastr = function
-  | [] -> None
-  | [a] -> Some a
-  | _ :: t -> lastr t
+let rec lastr = function [] -> None | [ a ] -> Some a | _ :: t -> lastr t
 
 let%test "test last function returns last element of list" =
-  last [] = None &&
-  last [1;2;3] = Some 3 &&
-  last ["a"; "b"] = Some "b"
+  last [] = None && last [ 1; 2; 3 ] = Some 3 && last [ "a"; "b" ] = Some "b"
 
 let%test "test last with recursion" =
-  lastr [] = None &&
-  lastr [1;2;3] = Some 3 &&
-  lastr ["a"; "b"] = Some "b"
+  lastr [] = None && lastr [ 1; 2; 3 ] = Some 3 && lastr [ "a"; "b" ] = Some "b"
 
 (*
    Last Two elements of a list
@@ -26,11 +17,24 @@ let%test "test last with recursion" =
    elements.
 *)
 let rec last_two = function
-  | [] | [_] -> None
-  | [a; b] -> Some (a, b)
+  | [] | [ _ ] -> None
+  | [ a; b ] -> Some (a, b)
   | _ :: t -> last_two t
 
 let%test "last two with recursion" =
-  last_two [] = None &&
-  last_two [1] = None &&
-  last_two [1;2;3;4] = Some (3, 4)
+  last_two [] = None
+  && last_two [ 1 ] = None
+  && last_two [ 1; 2; 3; 4 ] = Some (3, 4)
+
+(*
+   N'th element of a list
+*)
+let nth lst index =
+  let rec aux count = function
+    | [] -> None
+    | h :: t -> if count = index then Some h else aux (count + 1) t
+  in
+  aux 0 lst
+
+let%test "get the nth element of a list" =
+  nth [] 2 = None && nth [ 1; 2; 3 ] 1 = Some 2
